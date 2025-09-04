df58fa9445ca304ac54eb75f296fd957b6e321d8
eniitanadeleke <eniitanadeleke@gmail.com>
2025-09-04 15:59:35 +0100

    this is my second commit

---
+++ javascript.js
@@ -0,0 +1,30 @@
+/*const name = prompt("what is your name?")
+console.log(name);
+
+
+let jobTitle = prompt("what is your job title?")
+console.log(jobTitle)
+
+const question = confirm("are you sure?")
+console.log(question)
+
+alert("hello world")*/
+
+
+let num1 = 7;
+let num2 = 3;
+const name = "Eniitan";
+
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication  = num1 * num2;
+let division = num1 / num2;
+
+console.log(name)
+console.log("Addition", addition);
+console.log("subtraction", subtraction);
+console.log("Multiplication", multiplication);
+console.log("Division", division);
+
+console.log(`Hello ${name}, the result of num1 + num2 is ${addition}`);
